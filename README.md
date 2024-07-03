e4e0e8396b60b2ca65db73e1c1e7a433fd40133e
Adhou <101729542+Adhou@users.noreply.github.com>
2024-07-03 23:49:02 +0000

    'init'

---
+++ README.md
@@ -0,0 +1,8 @@
+Contact App Preview
+
+
+
+
+
+https://github.com/Nisha0202/ContactApp/assets/99580632/3c0aaddd-a3cf-4940-9c3d-335e551638b4
+
---
+++ app/src/main/java/com/example/mycalculator/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.mycalculator;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import java.util.Arrays;
+import java.util.Stack;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
